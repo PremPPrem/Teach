6970825aa63afac84553b6f401c2a630d1346c96
PremPPrem <onusoka.thaika@gmail.com>
2023-08-01 05:23:00 +0700

    edit

--- pages/components/Header.jsx
+++ pages/components/Header.jsx
@@ -4,7 +4,7 @@ export default function Header({name,image}) {
   return (
     <div className=' h-[50vh] relative bg-headImage  bg-center bg-cover bg-no-repeat '>
       <div className='w-full h-full flex items-center justify-center '>
-        <h1 className=' text-[4rem] text-white '>{name}</h1>
+        <h1 className=' text-[4rem] text-white text-center '>{name}</h1>
       </div>
       <img src={image} alt="" className='h-[50vh] absolute top-0 left-0 z-[-1] object-cover  w-full ' />
     </div>
